8edff3af9f5e235e64ecac1549690632ad4da7cf
LeonXZhou <leon.zhou@queensu.ca>
2021-11-05 03:37:30 +0000

    refactored and added unit testing with moch and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,27 @@
 const request = require('request');
-const fs = require('fs')
-const args = process.argv.slice(2);
-const catBreedSearch = 'https://api.thecatapi.com/v1/breeds/search'
-request(catBreedSearch + `?q=${args[0]}`, (error, response, body) => {
-  if (body === '[]') {
-    console.log('cat breed not found');
-  }
-  if (error) {
-    console.log('error:', error);
-  }
-  if (!(body === '[]') && !error) {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+const catBreedSearch = 'https://api.thecatapi.com/v1/breeds/search';
+const fetchBreedDescription = function(breedName, callback) {
+  request(catBreedSearch + `?q=${breedName}`, (error, response, body) => {
+    if (error === null && body !== '[]') {
+      callback(null,JSON.parse(body)[0].description);
+    }
+    if (body === '[]') {
+      callback('cat breed not found',null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
 
+
+// if (body === '[]') {
+//   console.log('cat breed not found');
+// }
+// if (error) {
+//   console.log('error:', error);
+// }
+// if (!(body === '[]') && !error) {
+//   const data = JSON.parse(body);
+//   console.log(data[0].description);
+// }
